25f72a58b5e1572381fa103b64d772bbe1e0d36b
thananchai2545 <ammarin.jo@gmail.com>
2025-08-10 13:40:15 +0700

    Update contact link text in header component

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -116,7 +116,7 @@ export default function Header() {
                       href="/contact"
                       className="block py-2 text-gray-700 hover:text-gray-900"
                     >
-                      Contact
+                      Contact Edit
                     </Link>
                   </NavigationMenuItem>
                 </NavigationMenuList>
