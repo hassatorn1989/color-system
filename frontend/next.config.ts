1012ded95a2b22fe8d21031987c65b50c1892c6c
hassatorn1989 <hassatorn2532@gmail.com>
2025-12-18 16:31:50 +0700

    chore: add pnpm workspace configuration with ignored built dependencies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 const path = require('path');
 const nextConfig: NextConfig = {
+  turbopack: {},
   output: "standalone",
   webpack: (config) => {
     // Resolve the `@` alias to the `lib` directory
-    config.resolve.alias['@'] = path.join(__dirname);
+    config.resolve.alias["@"] = path.join(__dirname);
     return config;
-  }
+  },
 };
 
 export default nextConfig;
